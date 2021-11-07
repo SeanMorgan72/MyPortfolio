3b7614964c4049373d433c3d2869deccfc5df7ca
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-11-06 22:24:30 -0500

    My Portfolio Website

---
+++ MyPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
